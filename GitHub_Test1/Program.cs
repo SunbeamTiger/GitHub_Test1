d506bfd92314490834643a1b61a6aae6c31dab1b
danhagen <danhagen@outlook.com>
2022-04-14 09:35:18 -0700

    Made change to USer Message

--- GitHub_Test1/Program.cs
+++ GitHub_Test1/Program.cs
@@ -6,7 +6,7 @@ namespace GitHub_Test1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from GitHub!!!");
         }
     }
 }
